48df699c763b54dcf5cc1034edb8b320aad1b2bb
phiphatX10 <phiphat.j@extend-it-resource.com>
2015-07-16 19:39:05 +0700

    initial

---
+++ iftverify/src/main/java/com/ktb/verify/iftVerifyResponse.java
@@ -0,0 +1,33 @@
+package com.ktb.verify;
+
+import java.io.Serializable;
+
+public class iftVerifyResponse implements Serializable{
+
+	private static final long serialVersionUID = 4447971315977609199L;
+
+	String TransSource;
+	String TransRefNo;
+	String TransDate;
+	
+	public String getTransSource() {
+		return TransSource;
+	}
+	public void setTransSource(String transSource) {
+		TransSource = transSource;
+	}
+	public String getTransRefNo() {
+		return TransRefNo;
+	}
+	public void setTransRefNo(String transRefNo) {
+		TransRefNo = transRefNo;
+	}
+	public String getTransDate() {
+		return TransDate;
+	}
+	public void setTransDate(String transDate) {
+		TransDate = transDate;
+	}
+	
+	
+}
---
+++ iftverify/src/main/java/com/ktb/verify/iftVerifyService.java
@@ -0,0 +1,11 @@
+package com.ktb.verify;
+
+public class iftVerifyService {
+
+	public iftVerifyResponse verify(iftVerifyRequest request){
+		System.out.println("verify process");
+		iftVerifyResponse response = new iftVerifyResponse();
+		return response;
+	}
+	
+}
